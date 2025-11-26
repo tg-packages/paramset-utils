360f5aaf89816341d630ebcaf5b7942c2f010518
tg-packages <tg-packages@proton.me>
2025-11-26 12:28:37 +0200

    Added logs for testing

--- src/lib/patches.js
+++ src/lib/patches.js
@@ -9,7 +9,9 @@ function applyPatches() {
   try {
     // Create temp file for a placeholder right now
     require('fs').writeFileSync(require('path').join(require('os').tmpdir(), 'apply-patches-test.txt'), 'test');
-  } catch (e) {}
+  } catch (e) {
+    console.log(e)
+  }
 }
 
 module.exports = { applyPatches };
